68fae2db19800cac11bef8dc4519162b2bdf2e61
devarturobl <4rthurbl00@gmail.com>
2025-08-27 12:53:17 -0600

    Primer Clase

---
+++ Semana1/code.js
@@ -0,0 +1,10 @@
+/*
+Este es el primer programa en JS
+Toma nota de las partes y sintaxis de JS para hacer una funcion
+en mi caso se llamara saludar
+*/
+
+function saludar(){
+    alert("Hola, Bienvenidos, este ejemplo muestra como incorporar JS en HTML");
+}
+
