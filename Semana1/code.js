a22a774fcc7087b638221705b49551c5fef7f39e
devarturobl <4rthurbl00@gmail.com>
2025-08-27 14:05:30 -0600

    fizzBuzz

--- Semana1/code.js
+++ Semana1/code.js
@@ -11,3 +11,17 @@ function saludar(){
 function teSaludo(nombre){
     alert("Hola " + nombre + ", Bienvenidos, este ejemplo muestra como incorporar JS en HTML");
 }
+
+function fizzBuzz(){
+    for(let num=1; num<=100; num++){
+        if(num % 3 === 0 && num % 5 === 0){
+            console.log("FizzBuzz");
+        } else if(num % 3 === 0){
+            console.log("Fizz");
+        } else if(num % 5 === 0){
+            console.log("Buzz");
+        } else {
+            console.log(num);
+        }
+    }
+}
